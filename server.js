47c2c890e690bb7c015bacf43fde547984a6134c
kishan7890 <kishansahu9348@gmail.com>
2025-07-13 21:41:48 +0530

    cors origin modified

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const myBookRoutes = require("./routes/myBookRoutes");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://book-library-backend-0vas.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
