185fce7fa79aabb6ddd9053b554e28544d74758d
kishan7890 <kishansahu9348@gmail.com>
2025-07-16 22:52:25 +0530

    vercel deployed link added to cors origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const myBookRoutes = require("./routes/myBookRoutes");
 
 const app = express();
 
-app.use(cors({ origin: "https://book-library-backend-0vas.onrender.com", credentials: true }));
+app.use(cors({ origin: "https://book-library-frontend-silk.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
