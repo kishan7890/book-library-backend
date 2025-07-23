07a4b344d4e857ff94ff0e459a537253bf270dda
kishan7890 <kishansahu9348@gmail.com>
2025-07-23 11:01:41 +0530

    origin added with https

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const myBookRoutes = require("./routes/myBookRoutes");
 
 const app = express();
 
-app.use(cors({ origin: "book-library-frontend-kishan-kumar-sahus-projects.vercel.app", credentials: true }));
+app.use(cors({ origin: "https://book-library-frontend-kishan-kumar-sahus-projects.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
