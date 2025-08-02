fa6d532d6d52068157dbce4ab69a793ca9a86399
kishan7890 <kishansahu9348@gmail.com>
2025-08-02 23:53:16 +0530

    modified upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../utils/cloudinary');
+const cloudinary = require('../utills/cloudinary');
 
 const storage = new CloudinaryStorage({
   cloudinary,
